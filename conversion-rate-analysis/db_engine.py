05e9bc4d5340e7f2942e2bdc1f53affd0ad0ad32
shayansm2 <shayansm09@gmail.com>
2023-10-30 13:07:13 +0330

    revert

--- conversion-rate-analysis/db_engine.py
+++ conversion-rate-analysis/db_engine.py
@@ -4,22 +4,22 @@ import sqlalchemy
 class DBEngine(object):
     @staticmethod
     def get_digikala_engine() -> sqlalchemy.Engine:
-        user = 's.shafieemoghadam'
-        password = 'WITd0Xnfnmos3XlvnHoBFA=='
-        host = '172.30.5.64'
-        port = '13306'
-        database = 'digikala'
+        user = 'user'
+        password = 'pass'
+        host = 'localhost'
+        port = '80'
+        database = 'db_log'
         assert user != "" and password != "", "please enter your user and password"
         return sqlalchemy.create_engine(
             f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}')
 
     @staticmethod
     def get_digikala_log_engine() -> sqlalchemy.Engine:
-        user = 'h.zand'
-        password = '951c898a1f9af97acec3fb9b8df4'
-        host = '172.30.5.214'
-        port = '13306'
-        database = 'digikala_log'
+        user = 'user'
+        password = 'pass'
+        host = 'localhost'
+        port = '80'
+        database = 'db'
         assert user != "" and password != "", "please enter your user and password"
         return sqlalchemy.create_engine(
             f'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}')
