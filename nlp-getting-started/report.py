5b1b886498c52dff517511212f3b704a9c1f395a
shayansm2 <shayansm09@gmail.com>
2023-11-03 15:56:30 +0330

    better visibility

--- nlp-getting-started/report.py
+++ nlp-getting-started/report.py
@@ -16,28 +16,28 @@ def report(metrics: MetricsCalculator):
     rect1 = Rectangle((0, 0), 1, 1, color=get_color(score))
     text1 = f'Accuracy : {score}'
     ax.add_patch(rect1)
-    ax.text(0.5, 0.5, text1, ha='center', va='center', color='white')
+    ax.text(0.5, 0.5, text1, ha='center', va='center')
 
     # create the second rectangle with text
     score = round(metrics.get_precision(), 3)
     rect2 = Rectangle((1.5, 0), 1, 1, color=get_color(score))
     text2 = f'Precision : {score}'
     ax.add_patch(rect2)
-    ax.text(2, 0.5, text2, ha='center', va='center', color='white')
+    ax.text(2, 0.5, text2, ha='center', va='center')
 
     # create the third rectangle with text
     score = round(metrics.get_recall(), 3)
     rect3 = Rectangle((3, 0), 1, 1, color=get_color(score))
     text3 = f'Recall : {score}'
     ax.add_patch(rect3)
-    ax.text(3.5, 0.5, text3, ha='center', va='center', color='white')
+    ax.text(3.5, 0.5, text3, ha='center', va='center')
 
     # create the fourth rectangle with text
     score = round(metrics.get_auc(), 3)
     rect4 = Rectangle((4.5, 0), 1, 1, color=get_color(score))
     text4 = f'AUC : {score}'
     ax.add_patch(rect4)
-    ax.text(5, 0.5, text4, ha='center', va='center', color='white')
+    ax.text(5, 0.5, text4, ha='center', va='center')
 
     # set the axis limits and remove the axis ticks
     ax.set_xlim(0, 6)
