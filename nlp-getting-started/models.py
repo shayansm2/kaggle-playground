53cc85cd9e59db8516ca271b7c67ad9b7d696bae
shayansm2 <shayansm09@gmail.com>
2023-11-04 14:29:10 +0330

    add workflows and models

---
+++ nlp-getting-started/models.py
@@ -0,0 +1,51 @@
+import abc
+import numpy as np
+import xgboost as xgb
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+
+class ModelInterface:
+    @abc.abstractmethod
+    def fit(self, x, y):
+        pass
+
+    @abc.abstractmethod
+    def predict(self, x):
+        pass
+
+    @abc.abstractmethod
+    def predict_proba(self, x):
+        pass
+
+
+class LogisticRegressionModel(LogisticRegression, ModelInterface):
+    pass
+
+
+class DecisionTreeModel(DecisionTreeClassifier, ModelInterface):
+    pass
+
+
+class RandomForestModel(RandomForestClassifier, ModelInterface):
+    pass
+
+
+class XGBoostModel(ModelInterface):
+    def __init__(self):
+        self.xgb_model = None
+        self.xgb_params = {}
+
+    def fit(self, x, y):
+        d_train = xgb.DMatrix(x, y)
+        self.xgb_model = xgb.train(self.xgb_params, d_train)
+
+    def predict(self, x):
+        d_val = xgb.DMatrix(x)
+        return self.xgb_model.predict(d_val) >= 0.5
+
+    def predict_proba(self, x):
+        d_val = xgb.DMatrix(x)
+        prob = self.xgb_model.predict(d_val)
+        return np.column_stack((1 - prob, prob))
---
+++ nlp-getting-started/workflow.py
@@ -0,0 +1,32 @@
+from input_providers import InputProviderInterface
+from models import ModelInterface
+import pandas as pd
+from metrics import MetricsCalculator
+
+
+class TrainValidationWorkflow(object):
+    def __init__(
+            self,
+            model: ModelInterface,
+            input_provider: InputProviderInterface,
+            df_train: pd.DataFrame,
+            df_validation: pd.DataFrame = None
+    ):
+        self.model = model
+        self.input_provider = input_provider
+        self.df_train = df_train
+        self.df_validation = df_validation
+
+    def get_model(self):
+        self._train_flow()
+        return self.model
+
+    def get_metrics_calculator(self) -> MetricsCalculator:
+        self._train_flow()
+        assert self.df_validation is not None, 'you should provide the validation dataframe'
+        x_validation, y_validation = self.input_provider.get_test_inputs(self.df_validation)
+        return MetricsCalculator(y_validation, self.model.predict_proba(x_validation)[:, 1])
+
+    def _train_flow(self):
+        x_train, y_train = self.input_provider.get_train_inputs(self.df_train)
+        self.model.fit(x_train, y_train)
